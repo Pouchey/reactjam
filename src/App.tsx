3bc518816dad34dabd57710e3fa0814bf391a8c6
Théophile <theophilerouchet@gmail.com>
2023-07-20 18:33:55 +0200

    test init config

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,20 @@
+import { useEffect, useState } from 'react';
+import { GameState } from './logic/logic';
+
 const App = () => {
+  const [game, setGame] = useState<GameState>();
+  useEffect(() => {
+    Rune.initClient({
+      onChange: ({ newGame }) => {
+        setGame(newGame);
+      },
+    });
+  }, []);
+
+  if (!game) {
+    return <div>Loading...</div>;
+  }
+
   return <div>App</div>;
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import rune from "vite-plugin-rune"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import rune from 'vite-plugin-rune';
 
 export default defineConfig({
-  plugins: [react(),rune({ logicPath: "./src/logic/logic.ts" })],
-})
-
+  base: '',
+  plugins: [react(), rune({ logicPath: './src/logic/logic.ts' })],
+});
